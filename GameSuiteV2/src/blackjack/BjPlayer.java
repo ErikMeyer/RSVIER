c2a25a67caa19e1fa943a040b0cef65e01cab64e
Erik Meyer <e.meyer.msc@gmail.com>
2014-07-23 16:44:40 +0200

    working on blackjack AI

--- GameSuiteV2/src/blackjack/BjPlayer.java
+++ GameSuiteV2/src/blackjack/BjPlayer.java
@@ -2,17 +2,45 @@ package blackjack;
 
 import java.util.ArrayList;
 
+/**
+ * @author EM
+ *
+ */
+/**
+ * @author EM
+ *
+ */
 public class BjPlayer { //Possibly extends Player/Speler class from main menu package
 	private String name;
-	private ArrayList<ArrayList<Card>> aryHands;
+	private boolean ai;
+	private ArrayList<ArrayList<Card>> aryHands = new ArrayList<ArrayList<Card>>();
+	private int bankRoll;
 	
 	//Constructor
-	public BjPlayer(String name) {
+	public BjPlayer(String name, int bankRoll, boolean ai) {
 		this.name = name;
+		this.ai = ai;
+		this.bankRoll = bankRoll;
+	}
+	
+	public int getBankroll() {
+		return bankRoll;
+	}
+	
+	public boolean getAI() {
+		return ai;
+	}
+	
+	public void setAI(boolean ai) {
+		this.ai = ai;
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	public ArrayList<ArrayList<Card>> getHands() {
@@ -21,6 +49,10 @@ public class BjPlayer { //Possibly extends Player/Speler class from main menu pa
 	
 	public ArrayList<Card> getHand(int hand) {
 		return aryHands.get(hand);
+	}
+	
+	public void addHand(ArrayList<Card> hand) {
+		aryHands.add(hand);
 	}
 	
 	/**
@@ -37,14 +69,71 @@ public class BjPlayer { //Possibly extends Player/Speler class from main menu pa
 	 * @return
 	 */
 	public int getValue(int hand) {
-		ArrayList<Card> aryHand = aryHands.get(hand);
 		int total = 0;
-		for (Card card: aryHand) {
-			if (((total + card.getValue()) > 21) & (card.getValue() == 11)) {
-				card.setValue(1);
-			}
+		for (Card card: aryHands.get(hand)) {
 			total = total + card.getValue();
-		}		
+		}
 		return total;
+	}
+	
+	/**
+	 * This method executes all the actions in the player's turn.
+	 * It contains the AI.
+	 * 
+	 * Actions:
+	 * Check for blackjack
+	 * Check for split-options
+	 * Check if hand value is equal or over 17 (if so pass, else call)
+	 * 
+	 * @param master
+	 */
+	public void doTurn(Blackjack master) {
+		
+		getHand(0).clear();
+		getHand(0).add(new Card("AH",11));
+		getHand(0).add(new Card("AC",11));
+		
+		for (ArrayList<Card> hand: aryHands) {
+			System.out.println("Turn "+master.getTurn());
+			System.out.println("My name is "+name);
+			System.out.println("Hand "+aryHands.indexOf(hand));
+			System.out.println("My first card is "+getHand(0).get(0).getName());
+			System.out.println("My second card is "+getHand(0).get(1).getName());
+			System.out.println("Total value comes to "+getValue(0));
+			
+			//Check for blackjack
+			/*
+			if (getValue(hand.get(aryHands.indexOf(hand)).getValue())==21) {
+				System.out.println("Blackjack baby!");
+				//Actions
+			}
+			*/
+			//Check for split
+			if ((hand.get(0).getValue()==11) & (hand.get(1).getValue()==11)) {
+				System.out.println("I have two aces, so I'm going to split");
+				
+				ArrayList<Card> newHand = new ArrayList<Card>();
+				newHand.add(hand.get(1)); //Add the second card of the current hand to a new hand
+				newHand.add(master.drawCard()); //Add a new second card to the new hand
+				hand.remove(1); //Remove the second card from the current hand
+				hand.add(master.drawCard()); //Add a new second card to the current hand
+				//addHand(newHand); //Add the new hand  <== Problematic!
+			}
+		}
+		
+		
+		
+		
+		if (name.equals("Bank")) {
+			master.endTurn();
+		}
+		else {
+			master.nextTurn();
+		}
+		 
+	}
+	
+	public void cleanUp() {
+		aryHands = new ArrayList<ArrayList<Card>>(); //Removes any hands (and cards in those hands)
 	}
 }
--- GameSuiteV2/src/blackjack/Blackjack.java
+++ GameSuiteV2/src/blackjack/Blackjack.java
@@ -1,19 +1,59 @@
 package blackjack;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * @author EM
  * This is the primary class for the blackjack package.
  * It generates a playing deck with 3 to 6 sets of 52 cards.
  * It creates blackjack player objects to keep track of hands.
+ * 
+ * Chronology
+ * 
+ * Player selects settings for new game
+ * Player clicks start
+ * Deck is generated according to settings
+ * Players are generated according to settings
+ * Cards are shuffled and dealt to players
+ * Turn is pushed to first player
+ * Player object executes actions and pushes turn to next player
+ * So forth until bank, after bank's turn the round ends
+ * Money is transferred
+ * Hands are destroyed, new deck is generated, shuffled, and dealt, etc. 
  */
 public class Blackjack {
-	public ArrayList<Card> aryDeck; //Make private after testing.
-	
+	private ArrayList<Card> aryDeck = new ArrayList<Card>();
+	private ArrayList<BjPlayer> aryPlayers = new ArrayList<BjPlayer>();
+	private int whoTurn = -1;
+		
 	//Constructor
 	public Blackjack() {
 		aryDeck = genDeck(1); //For testing purposes 1 deck.
+		aryPlayers.add(new BjPlayer("AI player one",9999,true));
+		aryPlayers.add(new BjPlayer("Bank",9999,true));
+		newRound();
+		nextTurn(); //Starts game
+	}
+	
+	public int getTurn() {
+		return whoTurn;
+	}
+	
+	public void setTurn(int turn) {
+		this.whoTurn = turn;
+	}
+	
+	public ArrayList<Card> getDeck() {
+		return aryDeck;
+	}
+	
+	public ArrayList<BjPlayer> getPlayers() {
+		return aryPlayers;
+	}
+	
+	public void addPlayer(String name, int bankRoll, boolean ai) {
+		aryPlayers.add(new BjPlayer(name,bankRoll,ai));
 	}
 	
 	//First generates one normal deck (52), 
@@ -34,6 +74,42 @@ public class Blackjack {
 				}
 			}
 		}
+		Collections.shuffle(tmpDeck);
 		return tmpDeck;
+	}
+	
+	public void nextTurn() {
+		whoTurn++;
+		BjPlayer player = aryPlayers.get(whoTurn);
+		player.doTurn(this);
+	}
+	
+	public void endTurn() {
+		//End of turn cleanup
+		whoTurn = -1;
+		for (BjPlayer player: aryPlayers) {
+			player.cleanUp();
+		}
+	}
+	
+	public Card drawCard() {
+		Card card = aryDeck.get(0);
+		aryDeck.remove(0);
+		return card;
+	}
+	
+	/**
+	 * Shuffles the deck and deals the cards
+	 */
+	public void newRound() {
+		for (int i = 0; i <= 3; i++) Collections.shuffle(aryDeck);
+		for (BjPlayer player: aryPlayers) {
+			ArrayList<Card> hand = new ArrayList<Card>();
+			for (int j = 0; j < 2; j++) {
+				hand.add(aryDeck.get(0));
+				aryDeck.remove(0);
+			}
+			player.addHand(hand);
+		}
 	}
 }
